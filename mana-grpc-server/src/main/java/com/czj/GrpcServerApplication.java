3887353da45f2154ab9cb8e0c8a89d2e33840ad1
caozhengjie <caozhengjie@youbanganda.com>
2019-04-02 15:04:55 +0800

    grpc 提交 自动生成代码

---
+++ mana-grpc-server/src/main/java/com/czj/GrpcServerApplication.java
@@ -0,0 +1,15 @@
+package com.czj;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class GrpcServerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(GrpcServerApplication.class);
+    }
+}
