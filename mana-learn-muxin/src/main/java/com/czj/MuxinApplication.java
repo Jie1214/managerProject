d20b3aaad671799464eb7e374692c437fa1bc671
caozhengjie <caozhengjie@youbanganda.com>
2019-06-14 15:44:00 +0800

    项目建成

--- mana-learn-muxin/src/main/java/com/czj/MuxinApplication.java
+++ mana-learn-muxin/src/main/java/com/czj/MuxinApplication.java
@@ -2,15 +2,21 @@ package com.czj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Hello world!
  *
  */
+@ComponentScan(basePackages = {"com.czj"})
+
 @SpringBootApplication
+// 扫描mybatis mapper包路径
+//@MapperScan(basePackages="com.czj.dao")
+// 扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
+//@ComponentScan(basePackages= {"com.czj", "org.n3r.idworker"})
+@ComponentScan(basePackages= {"com.czj"})
 public class MuxinApplication
 {
     public static void main( String[] args )
